120054af8249ca2bfb5a18855230b3fd0de9b683
Karthik Vignesh <thekarthiq@gmail.com>
2022-04-17 02:02:43 +0530

    fixed : gateway service fetch properties from config service.

--- gateway/src/main/java/com/vdevelopers/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/vdevelopers/gateway/GatewayApplication.java
@@ -1,9 +1,15 @@
 package com.vdevelopers.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@RefreshScope
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
